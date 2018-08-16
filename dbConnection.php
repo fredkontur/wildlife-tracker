fe7af5ba931a0dcd2cddd7d213af64d132e96a75
fredkontur <fredkontur@gmail.com>
2018-08-15 20:59:11 -0400

    database connection

--- dbConnection.php
+++ dbConnection.php
@@ -1,4 +1,9 @@
 <?php
+    if(count(get_included_files()) === 1) {
+        header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
+        exit;
+    }
+
     $url = YOUR_URL; // Enter the URL for your database here
     $user = YOUR_USER_NAME; // Enter the username for your database here
     $pswrd = YOUR_PASSWORD; // Enter the password for your database here
